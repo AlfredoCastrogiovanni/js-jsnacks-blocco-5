76fc1f44b5798f8f83bce3f10facfb15ba99ba74
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-11-28 12:11:45 +0100

    snack3: feature indice aggiunta

--- snack3/js/index.js
+++ snack3/js/index.js
@@ -1,14 +1,18 @@
 
 const browser = {
-    "tab": ["Facebook", "GitHub", "Gmail", "Instagram"],
+    "tab": ["Facebook", "GitHub", "Gmail", "Instagram", "TikTok"],
     "activeTab": 0
 }
 
-const socialList = ["Facebook", "Instagram"];
+const socialList = ["Facebook", "Instagram", "TikTok"];
 
 browser.tab.forEach((element, index, array) => {
     socialList.includes(element) ? array.splice(index, 1) : 0;
-    console.log(array);
+
+    if(socialList.includes(element)) {
+        array.splice(index, 1);
+        index == browser.activeTab ? browser.activeTab++ : index;
+    }
 });
 
 console.log(browser);
