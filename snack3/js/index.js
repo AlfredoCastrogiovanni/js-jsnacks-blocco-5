3368a3df712cc19e6b857134b45632f679a93617
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-11-28 12:12:45 +0100

    snack3: correzione

--- snack3/js/index.js
+++ snack3/js/index.js
@@ -1,14 +1,12 @@
 
 const browser = {
-    "tab": ["Facebook", "GitHub", "Gmail", "Instagram", "TikTok"],
+    "tab": ["Facebook", "GitHub", "Gmail", "Instagram"],
     "activeTab": 0
 }
 
-const socialList = ["Facebook", "Instagram", "TikTok"];
+const socialList = ["Facebook", "Instagram"];
 
 browser.tab.forEach((element, index, array) => {
-    socialList.includes(element) ? array.splice(index, 1) : 0;
-
     if(socialList.includes(element)) {
         array.splice(index, 1);
         index == browser.activeTab ? browser.activeTab++ : index;
